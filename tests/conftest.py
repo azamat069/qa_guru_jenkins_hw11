f8b2321d1970a8867d333b43de40723e5057ef2a
Azamat Abdurashidov <royalty5611@gmail.com>
2024-09-10 22:56:07 +0300

    add files

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,22 +4,13 @@ from selene import browser
 from utils import attach
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-# from dotenv import load_dotenv
-# import os
 
-# @pytest.fixture(scope="session", autouse=True)
-# def load_env():
-#     load_dotenv()
-#
-# selenoid_login = os.getenv("SELENOID_LOGIN")
-# selenoid_pass = os.getenv("SELENOID_PASS")
-# selenoid_url = os.getenv("SELENOID_URL")
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
     browser.config.base_url = 'https://demoqa.com'
-    browser.config.window_height = '1080'
-    browser.config.window_width = '1920'
+    # browser.config.window_height = '1080'
+    # browser.config.window_width = '1920'
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
