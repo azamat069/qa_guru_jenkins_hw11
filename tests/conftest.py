ced147b0b586a49b7cff04fc5ac6f33eb552710e
Azamat Abdurashidov <royalty5611@gmail.com>
2024-09-11 00:12:56 +0300

    add files

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,25 +3,11 @@ from selene import browser
 from utils import attach
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from dotenv import load_dotenv
-import os
-
-
-@pytest.fixture(scope="session", autouse=True)
-def load_env():
-    load_dotenv()
-
-
-selenoid_login = os.getenv("SELENOID_LOGIN")
-selenoid_pass = os.getenv("SELENOID_PASS")
-selenoid_url = os.getenv("SELENOID_URL")
 
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
     browser.config.base_url = 'https://demoqa.com'
-    # browser.config.window_height = '1080'
-    # browser.config.window_width = '1920'
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
@@ -34,7 +20,7 @@ def browser_management():
 
     options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
-        command_executor=f"https://{selenoid_login}:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options)
 
     browser.config.driver = driver
