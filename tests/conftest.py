6028325c5c475c7f832d9b41d003206f52af26ca
Azamat Abdurashidov <royalty5611@gmail.com>
2024-09-11 00:51:17 +0300

    add files

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,11 @@ from selene import browser
 from utils import attach
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
+from dotenv import load_dotenv
+import os
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
@@ -17,14 +21,17 @@ def browser_management():
             "enableVideo": True
         }
     }
+    selenoid_login = os.getenv("SELENOID_LOGIN")
+    selenoid_pass = os.getenv("SELENOID_PASS")
+    selenoid_url = os.getenv("SELENOID_URL")
 
     options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
         options=options)
 
     browser.config.driver = driver
-    yield browser
+    yield
 
     attach.add_screenshot(browser)
     attach.add_logs(browser)
